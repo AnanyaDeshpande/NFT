00463a4d5a4cbf5b49d97ab13309ca687c816e52
Ananya Deshpande <01fe21bcs059@kletech.ac.in>
2024-06-10 12:08:27 +0530

    halfcssdone

--- src/components/ConfirmTicket.jsx
+++ src/components/ConfirmTicket.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import "./ConfirmTicket.css";
+import logo1 from './strlogo.png';
 
 function ConfirmTicket() {
   const location = useLocation();
@@ -13,12 +14,24 @@ function ConfirmTicket() {
   }
 
   return (
+    <div className="home-container">
+    <nav className="navbar">
+    <img src={logo1} alt="teamlogo" className="nav-logo-image" />
+
+      <div className="navbar-logo">IPL Ticket Booking</div>
+      <ul className="navbar-links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/About">About</a></li>
+        <li><a href="/tickets">Tickets</a></li>
+      </ul>
+    </nav>
     <div className="confirmation-container">
       <h1>Ticket Confirmation</h1>
-      <p>Match ID: {matchId}</p>
-      <p>Price: {price} IPL Tokens</p>
-      <p>Transaction Hash: {transactionHash}</p>
-      <button onClick={() => navigate('/tickets')}>Back to Tickets</button>
+      <p><strong>Match ID:</strong> {matchId}</p>
+      <p><strong>Price: </strong>{price} IPL Tokens</p>
+      <p><strong>Transaction Hash: </strong>{transactionHash}</p>
+      {/* <button onClick={() => navigate('/tickets')}>Back to Tickets</button> */}
+    </div>
     </div>
   );
 }
--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -6,6 +6,9 @@ import IPLTicketing from "./iplticket.json"; // Import the IPLTicketing contract
 import TicketingNFT from "./TicketingNFT.json"; // Import the TicketingNFT contract JSON
 import "./Tickets.css";
 
+import logo1 from './strlogo.png';
+
+
 function Tickets() {
   const { selectedAccount } = useContext(AccountContext);
   const [provider, setProvider] = useState(null);
@@ -17,26 +20,26 @@ function Tickets() {
   const navigate = useNavigate();
 
   const matches = [
-    { id: 1, matchName: 'Mumbai Indians vs Chennai Super Kings', venue: 'Wankhede Stadium, Mumbai', price: 1, nftToken: 1 },
-    { id: 2, matchName: 'Royal Challengers Bangalore vs Sunrisers Hyderabad', venue: 'M. Chinnaswamy Stadium, Bangalore', price: 5, nftToken: 5 },
-    { id: 3, matchName: 'Delhi Capitals vs Kolkata Knight Riders', venue: 'Arun Jaitley Stadium, Delhi', price: 10, nftToken: 10 },
-    { id: 4, matchName: 'Punjab Kings vs Rajasthan Royals', venue: 'Punjab Cricket Association IS Bindra Stadium, Mohali', price: 5, nftToken: 5 },
-    { id: 5, matchName: 'Chennai Super Kings vs Mumbai Indians', venue: 'MA Chidambaram Stadium, Chennai', price: 15, nftToken: 15 },
-    { id: 6, matchName: 'Sunrisers Hyderabad vs Royal Challengers Bangalore', venue: 'Rajiv Gandhi International Stadium, Hyderabad', price: 10, nftToken: 10 },
-    { id: 7, matchName: 'Kolkata Knight Riders vs Delhi Capitals', venue: 'Eden Gardens, Kolkata', price: 15, nftToken: 15 },
-    { id: 8, matchName: 'Rajasthan Royals vs Punjab Kings', venue: 'Sawai Mansingh Stadium, Jaipur', price: 15, nftToken: 5 },
-    { id: 9, matchName: 'Mumbai Indians vs Royal Challengers Bangalore', venue: 'Wankhede Stadium, Mumbai', price: 15, nftToken: 15 },
-    { id: 10, matchName: 'Chennai Super Kings vs Sunrisers Hyderabad', venue: 'MA Chidambaram Stadium, Chennai', price: 10, nftToken: 10 },
-    { id: 11, matchName: 'Delhi Capitals vs Punjab Kings', venue: 'Arun Jaitley Stadium, Delhi', price: 5, nftToken: 5 },
-    { id: 12, matchName: 'Kolkata Knight Riders vs Rajasthan Royals', venue: 'Eden Gardens, Kolkata', price: 5, nftToken: 5 },
-    { id: 13, matchName: 'Royal Challengers Bangalore vs Mumbai Indians', venue: 'M. Chinnaswamy Stadium, Bangalore', price: 20, nftToken: 20 },
-    { id: 14, matchName: 'Sunrisers Hyderabad vs Chennai Super Kings', venue: 'Rajiv Gandhi International Stadium, Hyderabad', price: 15, nftToken: 15 },
-    { id: 15, matchName: 'Punjab Kings vs Delhi Capitals', venue: 'Punjab Cricket Association IS Bindra Stadium, Mohali', price: 10, nftToken: 10 },
-    { id: 16, matchName: 'Rajasthan Royals vs Kolkata Knight Riders', venue: 'Sawai Mansingh Stadium, Jaipur', price: 5, nftToken: 5 },
-    { id: 17, matchName: 'Mumbai Indians vs Delhi Capitals', venue: 'Wankhede Stadium, Mumbai', price: 10, nftToken: 10 },
-    { id: 18, matchName: 'Royal Challengers Bangalore vs Chennai Super Kings', venue: 'M. Chinnaswamy Stadium, Bangalore', price: 25, nftToken: 25 },
-    { id: 19, matchName: 'Sunrisers Hyderabad vs Rajasthan Royals', venue: 'Rajiv Gandhi International Stadium, Hyderabad', price: 15, nftToken: 15 },
-    { id: 20, matchName: 'Kolkata Knight Riders vs Punjab Kings', venue: 'Eden Gardens, Kolkata', price: 10, nftToken: 10 }
+    { id: 1, matchName: 'Mumbai Indians\nvs\nChennai Super Kings', venue: 'Wankhede Stadium, Mumbai', price: 1, nftToken: 1 },
+    { id: 2, matchName: 'Royal Challengers Bangalore\nvs\nSunrisers Hyderabad', venue: 'M. Chinnaswamy Stadium, Bangalore', price: 5, nftToken: 5 },
+    { id: 3, matchName: 'Delhi Capitals\nvs\nKolkata Knight Riders', venue: 'Arun Jaitley Stadium, Delhi', price: 10, nftToken: 10 },
+    { id: 4, matchName: 'Punjab Kings\nvs\nRajasthan Royals', venue: 'Punjab Cricket Association IS Bindra Stadium, Mohali', price: 5, nftToken: 5 },
+    { id: 5, matchName: 'Chennai Super Kings\nvs\nMumbai Indians', venue: 'MA Chidambaram Stadium, Chennai', price: 15, nftToken: 15 },
+    { id: 6, matchName: 'Sunrisers Hyderabad\nvs\nRoyal Challengers Bangalore', venue: 'Rajiv Gandhi International Stadium, Hyderabad', price: 10, nftToken: 10 },
+    { id: 7, matchName: 'Kolkata Knight Riders\nvs\nDelhi Capitals', venue: 'Eden Gardens, Kolkata', price: 15, nftToken: 15 },
+    { id: 8, matchName: 'Rajasthan Royals\nvs\nPunjab Kings', venue: 'Sawai Mansingh Stadium, Jaipur', price: 15, nftToken: 5 },
+    { id: 9, matchName: 'Mumbai Indians\nvs\nRoyal Challengers Bangalore', venue: 'Wankhede Stadium, Mumbai', price: 15, nftToken: 15 },
+    { id: 10, matchName: 'Chennai Super Kings\nvs\nSunrisers Hyderabad', venue: 'MA Chidambaram Stadium, Chennai', price: 10, nftToken: 10 },
+    { id: 11, matchName: 'Delhi Capitals\nvs\nPunjab Kings', venue: 'Arun Jaitley Stadium, Delhi', price: 5, nftToken: 5 },
+    { id: 12, matchName: 'Kolkata Knight Riders\nvs\nRajasthan Royals', venue: 'Eden Gardens, Kolkata', price: 5, nftToken: 5 },
+    { id: 13, matchName: 'Royal Challengers Bangalore\nvs\nMumbai Indians', venue: 'M. Chinnaswamy Stadium, Bangalore', price: 20, nftToken: 20 },
+    { id: 14, matchName: 'Sunrisers Hyderabad\nvs\nChennai Super Kings', venue: 'Rajiv Gandhi International Stadium, Hyderabad', price: 15, nftToken: 15 },
+    { id: 15, matchName: 'Punjab Kings\nvs\nDelhi Capitals', venue: 'Punjab Cricket Association IS Bindra Stadium, Mohali', price: 10, nftToken: 10 },
+    { id: 16, matchName: 'Rajasthan Royals\nvs\nKolkata Knight Riders', venue: 'Sawai Mansingh Stadium, Jaipur', price: 5, nftToken: 5 },
+    { id: 17, matchName: 'Mumbai Indians\nvs\nDelhi Capitals', venue: 'Wankhede Stadium, Mumbai', price: 10, nftToken: 10 },
+    { id: 18, matchName: 'Royal Challengers Bangalore\nvs\nChennai Super Kings', venue: 'M. Chinnaswamy Stadium, Bangalore', price: 25, nftToken: 25 },
+    { id: 19, matchName: 'Sunrisers Hyderabad\nvs\nRajasthan Royals', venue: 'Rajiv Gandhi International Stadium, Hyderabad', price: 15, nftToken: 15 },
+    { id: 20, matchName: 'Kolkata Knight Riders\nvs\nPunjab Kings', venue: 'Eden Gardens, Kolkata', price: 10, nftToken: 10 }
   ];
 
   useEffect(() => {
@@ -158,6 +161,8 @@ function Tickets() {
   return (
     <div className="home-container">
       <nav className="navbar">
+      <img src={logo1} alt="teamlogo" className="nav-logo-image" />
+
         <div className="navbar-logo">IPL Ticket Booking</div>
         <ul className="navbar-links">
           <li><a href="/">Home</a></li>
@@ -165,18 +170,27 @@ function Tickets() {
           <li><a href="/tickets">Tickets</a></li>
         </ul>
       </nav>
-      <section className="tickets-content">
-        <div className="sort-container">
+
+
+
+      <section className="sortingsec">
+      <div className="sort-container">
           <select className="sort-dropdown" onChange={(e) => setSortAsc(e.target.value === 'asc')}>
-            <option value="asc">Sort by Price: Ascending</option>
-            <option value="desc">Sort by Price: Descending</option>
+            <option value="asc">Sort by Price: Low to High</option>
+            <option value="desc">Sort by Price: High to Low</option>
           </select>
         </div>
-        <br></br>
-        <br></br>
+      </section>
+
+
+
+      <section className="tickets-content">
+
         {sortedMatches.map(match => (
           <div key={match.id} className="ticket-card">
-            <h2>{match.matchName}</h2>
+            <h2>{match.matchName.split('\n').map((line, index) => (
+        <div key={index}>{line}</div>
+      ))}</h2>
             <p>Venue: {match.venue}</p>
             <p>Price: {match.price} IPL Tokens</p>
             <p>NFT Token: {match.nftToken}</p>
